3be8a0f70ab41858e2c5ea82a885ead513d5e606
unknown <you@example.com>
2019-03-21 19:38:48 +0300

    My First Commit

---
+++ resources/js/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                    <h1>Footer</h1>
+                        </div>
+                    </div>
+             </div>
+           
+        );
+    }
+}
---
+++ resources/js/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                    <h1>Header</h1>
+                        </div>
+                    </div>
+             </div>
+           
+        );
+    }
+}
